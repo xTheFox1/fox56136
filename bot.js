863578ef3461d9e9cc3f2e132c44f65d58eabef6
xTheFox1 <38634034+xTheFox1@users.noreply.github.com>
2018-12-07 00:03:10 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -10,10 +10,7 @@ client.on('message', message => {
       
   if (message.content.startsWith(adminprefix + 'ply')) {
     client.user.setGame(argresult);
-      message.channel.send(`**Done :white_check_mark:**`)
-  } else 
-     if (message.content === (adminprefix + "leave897807")) {
-    message.guild.leave();        
+      message.channel.send(`**Done :white_check_mark:**`) 
   } else  
   if (message.content.startsWith(adminprefix + 'wt')) {
   client.user.setActivity(argresult, {type:'WATCHING'});
@@ -26,15 +23,9 @@ client.on('message', message => {
   if (message.content.startsWith(adminprefix + 'st')) {
     client.user.setGame(argresult, "https://www.twitch.tv/dream");
       message.channel.send(`**Done :white_check_mark: **`)
-  }
-  if (message.content.startsWith(adminprefix + 'setname7774')) {
-  client.user.setUsername(argresult).then
-      message.channel.send(`Changing The Name To ..**${argresult}** `)
-} else
-if (message.content.startsWith(adminprefix + 'setava4747')) {
-  client.user.setAvatar(argresult);
-    message.channel.send(`Changing The Avatar To :**${argresult}** `);
 }
+    
+    
 });
 
 
